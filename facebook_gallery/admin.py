c921a9f6dbaceb544ce6d7f1e258727cbd4c786b
Viktor Nagy <viktor.nagy@gmail.com>
2012-10-22 11:33:23 +0200

    Added adminplus action to fetch galleries.

--- facebook_gallery/admin.py
+++ facebook_gallery/admin.py
@@ -1,13 +1,42 @@
+from operator import attrgetter
+
+from django.shortcuts import redirect, render_to_response
+from django.core.urlresolvers import reverse
 from django.contrib import admin
+from django.template import RequestContext
 
 import models
 import facebook_api
+
+def fetch_albums(request, *args, **kwargs):
+    if request.method == 'POST':
+        import ipdb; ipdb.set_trace()   
+        if request.POST.get('page_id', False):
+            albums = facebook_api.fetch_albums(int(request.POST['page_id']))
+            self.message_user(request, "%d albums were fetched" % len(albums))
+        else:
+            self.message_user(request, "Please provide an object ID")
+        return redirect(reverse('admin:index'))
+    else:
+        return render_to_response('facebook_gallery/fetch_album_form.html', context_instance=RequestContext(request))
+admin.site.register_view('fetch_albums', fetch_albums)
+
+def delete_images(modeladmin, request, queryset):
+    """Deleted the images of the selected galleries"""
+    del_method = attrgetter('delete')
+    for gallery in queryset:
+        map(del_method, gallery.image_set.all())
+        gallery.count = 0
+        gallery.save()
+        modeladmin.message_user(request, "The images were deleted")
+    return redirect(reverse('admin:index'))
+delete_images.short_description = "Delete related images"
 
 class GalleryAdmin(admin.ModelAdmin):
     list_display = ['name', 'count', 'published']
     list_filter = ['published']
     ordering = ['created_time']
-    actions = ['fetch_images', 'publish', 'unpublish']
+    actions = ['fetch_images', 'publish', 'unpublish', delete_images]
 
     def fetch_images(self, request, queryset):
         for album in queryset:
